c32465205e933c4a55b5327b916643cc3edf05e8
Fahid Attique <fahad@techmatetech.com>
2022-07-08 01:40:42 +0800

    all code + pod specs and readme file (in-progress)

---
+++ QWarySurvey.podspec
@@ -0,0 +1,19 @@
+Pod::Spec.new do |s|
+  s.swift_version    = '5.0'
+  s.name             = 'QWarySurveySDK'
+  s.version          = '1.0.0'
+  s.summary          = 'QWarySurvey iOS SDK enables you to collect feedback from your mobile app. Embed the surveys in your iOS application seamlessly with few lines of code.'
+
+  s.description      = <<-DESC
+  QwarySurvey SDK
+                       DESC
+
+  s.homepage         = 'https://gitlab.com/Shehryar3/qwarysurvey/'
+  s.license          = { :type => 'Apache License, Version 2.0', :file => 'LICENSE' }
+  s.author           = { 'Fahid Attique' => 'fahad@techmatetech.com' }
+  s.source           = { :git => 'https://gitlab.com/Shehryar3/qwarysurvey.git', :tag => s.version.to_s }
+
+  s.ios.deployment_target = '13.0'
+  s.source_files = 'QWarySurvey/SDK/**/*.{swift}'
+
+end
---
+++ QWarySurvey/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  QWarySurvey
+//
+//  Created by fahid on 27/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, QWSurveyDelegate {
+
+    // MARK: Connection
+    @IBOutlet weak var qwSurveyView: QWSurveyView!
+
+    // MARK: Properties
+    var request: QWSurveyRequest {
+        var params = [String:String]()
+        params["email"] = "jondoe2@acmeinc.com"
+        params["planId"] = "trial2"
+        let request = QWSurveyRequest(scheme: nil, host: "survey.qwary.com", path: "/form/S_wSzSPnasH9Wc_FT15X0J1BuEcPl5gIZ99rQiotQa8=", params: params)
+        return request
+    }
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let qwSurvey = QWSurvey(request: request, delegate: self)
+        qwSurvey.scheduleSurvey(parent: self)
+    }
+    
+    // MARK: Actions
+    @IBAction func startFullscreenSurvey(_ sender: UIButton) {
+        let qwSurveyViewController = QWSurveyViewController(request: request, delegate: self)
+        present(qwSurveyViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showEmbeddedSurvey(_ sender: UIButton) {
+        qwSurveyView.loadSurvey(request: request, delegate: self)
+    }
+    
+    // MARK: Delegate
+    func didReceiveSurveyOutcome(_ state: QWSurveyState) {
+        print("Received outcome from survey: \(state)")
+    }
+    func didRedirectToURL(_ url: String) {
+        print("Redirected to url: \(url)")
+    }
+}
+
